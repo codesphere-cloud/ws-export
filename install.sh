6dd6afaa4e35289cb4c62890be8de7fb7abe7735
Nazım Umut Ekici <42119171+Emut@users.noreply.github.com>
2025-01-26 21:32:42 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# below steps work with an ext4 drive mounted at "/home/user/.local/share/containers/storage"
+
+nix-env -iA nixpkgs.podman
+PATH=$(pwd):$PATH
+
+
+
+# podman build -f Dockerfile -t umut:no-priv  --signature-policy ./policy.json
+
+# podman save umut:no-priv -o image.tar 
